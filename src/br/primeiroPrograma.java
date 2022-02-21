bece0b92dd94a0577237e7af66e906bad4431078
Ursulino Rocha <sulinocosta@gmail.com>
2022-02-20 23:22:22 -0300

    first commit

---
+++ src/br/primeiroPrograma.java
@@ -0,0 +1,17 @@
+package br;
+
+
+import br.model.GATO;
+
+public class primeiroPrograma {
+    public static void main(String[] args){
+        GATO gato = new GATO();
+
+        System.out.println(gato);
+
+       /* int a =5;
+        int b =3;
+        System.out.println("Hello World! " + (a+b));*/
+    }
+
+}
